388ea9bf97911dddf8ca3d835edc16d6ca21ce8b
Rayen Lee <rayenlee64@gmail.com>
2024-12-10 20:09:18 +1300

    Session Storage
    Implemented Session Storage to save the user data so that refreshes don't forget the user.
    
    Further improvement of this involves the incorporation of a refresh token for additional security.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,6 @@
+import { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
+import { useAtomValue } from "jotai";
 
 import Layout from "./components/Layout";
 import LandingDirectory from "./components/LandingDirectory";
@@ -6,7 +8,18 @@ import LoginPage from "./pages/LoginPage";
 import SignUpPage from "./pages/SignUpPage";
 import AdminDirectory from "./components/AdminDirectory";
 
+import { accessLevelAtom, accessTokenAtom } from "./atoms";
+
 const App = () => {
+  const accessLevel = useAtomValue(accessLevelAtom);
+  const accessToken = useAtomValue(accessTokenAtom);
+
+  // Save Access Level and Access Token to session storage whenever their state changes
+  useEffect(() => {
+    sessionStorage.setItem("accessLevel", accessLevel);
+    sessionStorage.setItem("accessToken", accessToken);
+  }, [accessLevel, accessToken]);
+
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
--- frontend/src/atoms.js
+++ frontend/src/atoms.js
@@ -1,4 +1,9 @@
 import { atom } from "jotai";
 
-export const accessTokenAtom = atom(null)
-export const accessLevelAtom = atom(null)
+export const accessTokenAtom = atom(
+  JSON.parse(sessionStorage.getItem("accessToken")) || null
+);
+
+export const accessLevelAtom = atom(
+  JSON.parse(sessionStorage.getItem("accessLevel")) || null
+);
