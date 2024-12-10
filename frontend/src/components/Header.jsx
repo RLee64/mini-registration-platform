80546e1a6e396af0cd9498d68336e3d669286e07
Rayen Lee <rayenlee64@gmail.com>
2024-12-10 21:31:07 +1300

    Title to Home
    Pressing the title/logo in the top left returns users to the landing page

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,22 +1,32 @@
+import { useNavigate } from "react-router-dom";
+
 import AuthButton from "./AuthButton";
 import logo from "../images/logo.png";
 
 const Header = () => {
-  // Styling for header tag found in index.css file
+  const navigate = useNavigate();
 
+  // Styling for header tag can be found in the index.css file
+
+  // Flex box for span that also shows indication of it being clickable
   const flexBoxWrapper = {
     display: "flex",
     alignItems: "center",
     gap: 10,
-  }
+    cursor: "pointer",
+  };
 
   const title = {
     marginBlock: 0,
   };
 
+  const toHome = () => {
+    navigate("/");
+  };
+
   return (
     <header>
-      <span style={flexBoxWrapper}>
+      <span style={flexBoxWrapper} onClick={toHome}>
         <img src={logo} alt="Logo" />
         <h1 style={title}>NZPMC</h1>
       </span>
