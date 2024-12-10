0a58d81963a03f32779e9414f8c34bd65cf77f0f
Rayen Lee <rayenlee64@gmail.com>
2024-12-10 15:46:13 +1300

    Footer fix
    Footer finally no longer says mmm potato

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   // Styling for footer tag found in index.css file
-  return <footer>mmm potato</footer>;
+  return <footer>© NZPMC 2024</footer>;
 };
 
 export default Footer
