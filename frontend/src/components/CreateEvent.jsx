b9491a43e5774b879cf38e7b7a835033c699d0e7
Rayen Lee <rayenlee64@gmail.com>
2024-12-10 18:04:00 +1300

    Create Event error handling & text area
    Added error handling for create event component, notably for when input fields are incomplete, a date is set in the past, or the server returns an error
    
    Also changed the description input to be of a textarea type

--- frontend/src/components/CreateEvent.jsx
+++ frontend/src/components/CreateEvent.jsx
@@ -1,8 +1,9 @@
 import { useAtomValue } from "jotai";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import { accessTokenAtom } from "../atoms";
 import platformApi from "../services/platform-api";
+import ErrorMessage from "./ErrorMessage";
 
 const CreateEvent = ({ events, setEvents }) => {
   const accessToken = useAtomValue(accessTokenAtom);
@@ -10,6 +11,21 @@ const CreateEvent = ({ events, setEvents }) => {
   const [newEventName, setNewEventName] = useState("");
   const [newEventDescription, setNewEventDescription] = useState("");
   const [newEventDate, setNewEventDate] = useState("");
+  const [errorMessage, setErrorMessage] = useState(null);
+
+  const descriptionStyle = {
+    width: "100%",
+    boxSizing: "border-box",
+    display: "block",
+    resize: "none",
+    fontFamily: "inherit",
+    margin: "10px 0 35px 0",
+  };
+
+  // Wipes error message when user starts re-entering information
+  useEffect(() => {
+    setErrorMessage(null);
+  }, [newEventName, newEventDescription, newEventDate]);
 
   const clearEventFields = () => {
     setNewEventName("");
@@ -21,6 +37,18 @@ const CreateEvent = ({ events, setEvents }) => {
     event.preventDefault();
     console.log("Creating event");
 
+    const today = new Date();
+    const eventDate = new Date(newEventDate);
+
+    if (!newEventName || !newEventDescription || !newEventDate) {
+      setErrorMessage("Please fill in all fields")
+      return
+    }
+    if (eventDate < today) {
+      setErrorMessage("Chosen date is in the past")
+      return
+    }
+
     const newEvent = {
       name: newEventName,
       description: newEventDescription,
@@ -30,7 +58,11 @@ const CreateEvent = ({ events, setEvents }) => {
     platformApi.postEvent(newEvent, accessToken).then((returnedEvent) => {
       setEvents(events.concat(returnedEvent));
       clearEventFields();
-    });
+    }).catch((error) => {
+      console.log(error)
+      setErrorMessage("Error - Event could not be created")
+    }
+    );
   };
 
   return (
@@ -45,12 +77,13 @@ const CreateEvent = ({ events, setEvents }) => {
           type="text"
         />
         <label htmlFor="eventDescription">Description</label>
-        <input
+        <textarea
+          style={descriptionStyle}
           id="eventDescription"
           value={newEventDescription}
           onChange={(event) => setNewEventDescription(event.target.value)}
-          type="text"
-        />
+          rows="4"
+        ></textarea>
         <label id="eventDate">Date</label>
         <input
           id="eventDate"
@@ -58,6 +91,7 @@ const CreateEvent = ({ events, setEvents }) => {
           onChange={(event) => setNewEventDate(event.target.value)}
           type="datetime-local"
         />
+        <ErrorMessage message={errorMessage} />
         <button type="submit">Submit</button>
       </form>
     </div>
